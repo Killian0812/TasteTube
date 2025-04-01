9986f89299cf8e5458df1fa7a46838a40eb8078e
Killian0812 <93117684+Killian0812@users.noreply.github.com>
2025-04-01 14:29:59 +0700

    change default env

--- lib/build_config.dart
+++ lib/build_config.dart
@@ -1,6 +1,6 @@
 class BuildConfig {
   static const String environment =
-      String.fromEnvironment('ENVIRONMENT', defaultValue: 'ngrok');
+      String.fromEnvironment('ENVIRONMENT', defaultValue: 'vercel');
   static const String version =
       String.fromEnvironment('VERSION', defaultValue: '1.0.0');
 }
