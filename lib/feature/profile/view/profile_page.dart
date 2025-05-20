23fddfce649fcd3af49c1124b04b0640b0fc8302
Killian0812 <ngcuong0812@gmail.com>
2025-05-20 16:31:40 +0700

    fix: add review view

--- lib/feature/profile/view/profile_page.dart
+++ lib/feature/profile/view/profile_page.dart
@@ -451,6 +451,28 @@ class ProfilePage extends StatelessWidget {
                         width: double.infinity,
                         height: double.infinity,
                       ),
+                      Positioned(
+                        bottom: 5,
+                        left: 5,
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.visibility,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              state.likedVideos[index].views.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 );
